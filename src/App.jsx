d200d1f2a4294838f2f0e41f4fc483b93499ef40
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2025-08-20 15:51:49 +0200

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard.jsx";
 import Player from "./components/Player.jsx";
 import "./App.css";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleSelectSquare(index) {
+    if (squares[index]) {
+      return;
+    }
+
+    const updatedSquares = [...squares];
+    updatedSquares[index] = activePlayer;
+    setSquares(updatedSquares);
+
+    setActivePlayer(prev => (prev === "X" ? "O" : "X"));
+  }
+
+  function handleRestart() {
+    setSquares(Array(9).fill(null));
+    setActivePlayer("X");
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player />
-          <Player />
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+
+        <GameBoard squares={squares} onSelectSquare={handleSelectSquare} />
+
+        <button onClick={handleRestart}>New Game</button>
       </div>
     </main>
   );
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,25 +1,10 @@
-import { useState } from "react";
-
-export default function GameBoard() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [activePlayer, setActivePlayer] = useState("X");
-
-  function handleSelectSquare(index) {
-    if (squares[index]) return;
-
-    const updatedSquares = [...squares];
-    updatedSquares[index] = activePlayer;
-    setSquares(updatedSquares);
-
-    setActivePlayer(prev => (prev === "X" ? "O" : "X"));
-  }
-
+export default function GameBoard({ squares, onSelectSquare }) {
   return (
-    <ol id="gameBoard">
+    <ol id="game-board">
       {squares.map((square, index) =>
         <li key={index}>
           <button
-            onClick={() => handleSelectSquare(index)}
+            onClick={() => onSelectSquare(index)}
             disabled={square !== null}
           >
             {square}
